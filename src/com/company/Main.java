7743ba32b006c5b90d128c94bdef6a46b0db1f5f
Aleks Nechayev <aleks.nechayev@gmail.com>
2016-12-11 13:29:17 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        int read =  System.in.read();
+        System.out.println("read =" + (char) read);
+    }
+}
